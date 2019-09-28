00349c95897a7fca06ca2c7417ef0d53351ade7a
Leslie Shen <38119800+shenles@users.noreply.github.com>
2019-09-28 15:19:39 -0700

    homepage

--- index.php
+++ index.php
@@ -1,5 +1,48 @@
-<?php
+<html lang="en">
+   <head>
+   <title>D&D Database</title>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-echo "Hello world!";
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
 
-?>
+   <link rel="stylesheet" type="text/css" href="./style.css"> 
+  
+   </head>
+    
+   <body>
+   
+   <h1>Dungeons & Dragons Database</h1>
+ 
+   <!-- bootstrap navbar --> 
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+         <a class="nav-link" href="#">Home</span></a></li>
+        <li class="nav-item">
+         <a class="nav-link" href="addchar.php">Create New Character</span></a></li>
+        <li class="nav-item">
+         <a class="nav-link" href="viewchars.php">View Saved Characters</span></a></li>
+        <li class="nav-item">
+         <a class="nav-link" href="classes.php">Classes</span></a></li>
+        <li class="nav-item">
+         <a class="nav-link" href="races.php">Races</span></a></li>
+        <li class="nav-item">
+         <a class="nav-link" href="weapons.php">Weapons</span></a></li>
+        <li class="nav-item">
+         <a class="nav-link" href="armor.php">Armor</span></a></li>
+        <li class="nav-item">
+         <a class="nav-link" href="spells.php">Spells</span></a></li>  
+      </ul>
+    </div>   
+   </nav>
+
+   <div class="homepage-info">
+   <p>Welcome to the Dungeons & Dragons Database. Here you can look up  
+      information from the D&D 5e basic rules.</p>
+   <p>Use the navigation bar to access the desired pages.</p> 
+   </div>
+
+   </body>
+</html>
