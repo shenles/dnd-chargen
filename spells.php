c19b4bd5faaa0d113a2235248b75230c175d5094
Leslie Shen <38119800+shenles@users.noreply.github.com>
2019-10-11 19:55:53 -0700

    styling

--- spells.php
+++ spells.php
@@ -172,7 +172,7 @@
         $result = $conn->query($sql);
 
         while ($row = $result->fetch_assoc()) {
-            echo "<tr>\n<td class="spellname" data-toggle=\"tooltip\" title=\"" . $row["descrip"] . "\">" . $row["name"] . "</td>\n<td>" . $row["level"] . "</td>\n<td>" . $row["school"] . "</td>\n<td>" . $row["casting"] . "</td>\n<td>" . $row["spellrange"] . "</td>\n<td>" . $row["components"] . "</td>\n<td>" . $row["cancast"] . "</td>\n<td>" . $row["duration"] . "</td>\n<td>" . $row["ritual"] . "</td>\n</tr>\n";
+            echo "<tr>\n<td data-toggle=\"tooltip\" title=\"" . $row["descrip"] . "\">" . "<span style=\"border-bottom: 1px dotted;\">" . $row["name"] . "</span></td>\n<td>" . $row["level"] . "</td>\n<td>" . $row["school"] . "</td>\n<td>" . $row["casting"] . "</td>\n<td>" . $row["spellrange"] . "</td>\n<td>" . $row["components"] . "</td>\n<td>" . $row["cancast"] . "</td>\n<td>" . $row["duration"] . "</td>\n<td>" . $row["ritual"] . "</td>\n</tr>\n";
 
         }
 
