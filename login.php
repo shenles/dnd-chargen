6769a1ae2afd7a0bcd04f9d84187aacdf13be690
Leslie Shen <38119800+shenles@users.noreply.github.com>
2019-11-04 20:07:19 -0800

    bootstrap buttons

--- login.php
+++ login.php
@@ -2,11 +2,13 @@
 
 session_start();
 
-echo "<html>\n<div class=\"homepage-info\">\n<form class=\"filterform\" action=\"login.php\" method=\"post\">";
+echo "<html>\n<head>\n<title>Login</title><link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\" integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\" crossorigin=\"anonymous\">\n<link rel=\"stylesheet\" type=\"text/css\" href=\"./style.css\"></head>";
+
+echo "<body><div class=\"homepage-info\">\n<form class=\"filterform\" action=\"login.php\" method=\"post\">";
 echo "<input type=\"text\" name=\"username\" placeholder=\"Enter your username\" required>";
 echo "<input type=\"password\" name=\"password\" placeholder=\"Enter your password\" required>";
 echo "<input type=\"submit\" value=\"Submit\">";
-echo "</form></div></html>";
+echo "</form></div></body></html>";
 
 if (!empty($_POST)) {
 
@@ -35,8 +37,8 @@ if (!empty($_POST)) {
         if ($row != NULL && $pass == $row["password"]) {
             $_SESSION['user_id'] = $user->id;
             echo "Login successful";
-            echo "<html><div class=\"homepage-info\"><a href=\"https://dnd-chargen.herokuapp.com/addchar.php\" class=\"btn btn-default\">Create character</a>";
-            echo "<a href=\"https://dnd-chargen.herokuapp.com/viewchars.php\" class=\"btn btn-default\">View saved characters</a></div></html>";
+            echo "<html><div class=\"homepage-info\"><a href=\"https://dnd-chargen.herokuapp.com/addchar.php\" class=\"btn btn-light\" role=\"button\">Create character</a>";
+            echo "<a href=\"https://dnd-chargen.herokuapp.com/viewchars.php\" class=\"btn btn-light\" role=\"button\">View saved characters</a></div></html>";
 
         } else {
             echo "Login failed";
