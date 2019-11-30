55b90bcd18d4b786ca4e08de94c140361212fd8f
Leslie Shen <38119800+shenles@users.noreply.github.com>
2019-11-30 13:21:31 -0800

    half elf stats

--- addchar.php
+++ addchar.php
@@ -369,6 +369,7 @@ if (isset($_SESSION['user_id'])) {
     }
 
     echo <<<EOT
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="addchar.js"></script>
     </body>
     </html>
