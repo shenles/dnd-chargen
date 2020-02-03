068e2e38c9785c88dd4e72701ef029ddc884b237
Leslie Shen <38119800+shenles@users.noreply.github.com>
2020-02-03 14:53:10 -0800

    update descriptions

--- backgrounds.php
+++ backgrounds.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
    <head>
-   <title>D&D Database</title>
+   <title>RPG Manager</title>
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
--- viewchars.php
+++ viewchars.php
@@ -7,13 +7,13 @@ if (isset($_SESSION['user_id'])) {
     echo <<<EOT
     <html>
     <head>
-    <title>D&D Database</title>
+    <title>RPG Manager</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="./style.css">
     </head>
 
     <body>
-    <h1>Viewing saved characters</h1>
+    <h1>Viewing your saved characters</h1>
     
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
      <div class="collapse navbar-collapse" id="navbarSupportedContent">
