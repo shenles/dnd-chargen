b1ee7684b5db380a9c016932c64a0cfc7980acfc
Leslie Shen <38119800+shenles@users.noreply.github.com>
2020-03-04 05:25:03 -0800

    submit

--- viewchars.js
+++ viewchars.js
@@ -15,9 +15,9 @@ function viewChar(toView) {
 
 	console.log(toView);
 	//document.getElementById('viewcharid').submit();
-	var myform = document.getElementById('viewcharid');
-	var submitForm = Object.getPrototypeOf(myform).submit;
-	submitForm.call(myform);
+	//var myform = document.getElementById('viewcharid');
+	//var submitForm = Object.getPrototypeOf(myform).submit;
+	//submitForm.call(myform);
 
 }
 
@@ -25,9 +25,9 @@ function editChar(toEdit) {
 
 	console.log(toEdit);
 	//document.getElementById('viewcharid').submit();
-	var myform2 = document.getElementById('viewcharid');
-	var submitForm = Object.getPrototypeOf(myform2).submit;
-	submitForm.call(myform2);
+	//var myform2 = document.getElementById('viewcharid');
+	//var submitForm = Object.getPrototypeOf(myform2).submit;
+	//submitForm.call(myform2);
 
 }
 
@@ -35,8 +35,8 @@ function levelUpChar(toLevelUp) {
 
 	console.log(toLevelUp);
 	//document.getElementById('viewcharid').submit();
-	var myform3 = document.getElementById('viewcharid');
-	var submitForm = Object.getPrototypeOf(myform3).submit;
-	submitForm.call(myform3);
+	//var myform3 = document.getElementById('viewcharid');
+	//var submitForm = Object.getPrototypeOf(myform3).submit;
+	//submitForm.call(myform3);
 
 }
